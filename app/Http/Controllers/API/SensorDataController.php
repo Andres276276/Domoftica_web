3f22c05f40ffd82945cbd7ed359614bf18d84246
Andres276276 <113720551+Andres276276@users.noreply.github.com>
2023-11-26 17:56:23 -0500

    Update SensorDataController.php

--- app/Http/Controllers/API/SensorDataController.php
+++ app/Http/Controllers/API/SensorDataController.php
@@ -10,9 +10,7 @@ class SensorDataController extends Controller
 {
     public function store(Request $request)
     {
-
         $data = $request->only(['humedad', 'temperatura', 'humedad_suelo', 'flujo_agua', 'fecha_hora']);
-
 
         TablaSensor::create($data);
 
