b01a8801e6de3105b3b044b363525a446bdb3d8d
Andres276276 <113720551+Andres276276@users.noreply.github.com>
2023-11-27 17:54:52 -0500

    Update UserController.php

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 use App\Models\User;
 
