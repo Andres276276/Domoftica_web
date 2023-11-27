6328d41b77308bfc298fb0b2cb5f80c435a6e1b9
Andres276276 <113720551+Andres276276@users.noreply.github.com>
2023-11-27 18:31:40 -0500

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,6 +45,11 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+
+ \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+    'throttle:api',
+    \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
